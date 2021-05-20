c364c0a3699dae276ebe8b24c71834fa892c4609
AgnesMoralesTH <agnesmoralesth@gmail.com>
2021-05-20 18:02:37 +0700

    Set up pillow and access to static folder, one hour into the tutorial, learned how to set up a photo model with a TAG model as a FOREIGNKEY. Will leave it here since the rest is allowing CRUD through the page and I don't need that on this project

--- photoshare/photoshare/settings.py
+++ photoshare/photoshare/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATIC_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
